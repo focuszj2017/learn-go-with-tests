a0158117a304f40404a5176d1b373d84aafbd969
focus <junzhou21@126.comm>
2023-12-25 00:13:08 +0800

    [helloworld] add spanish support

--- helloworld/hello.go
+++ helloworld/hello.go
@@ -3,14 +3,18 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
-func Hello(name string) string {
+func Hello(name string, lanuage string) string {
 	if name == "" {
 		name = "World"
+	}
+	if lanuage == "Spanish" {
+		return spanishHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "English"))
 }
