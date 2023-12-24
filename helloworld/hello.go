348a946ba6db55d5f545c7649bdcce718c9c183a
focus <junzhou21@126.comm>
2023-12-25 00:15:05 +0800

    [helloworld] add french support

--- helloworld/hello.go
+++ helloworld/hello.go
@@ -2,16 +2,24 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
 func Hello(name string, lanuage string) string {
 	if name == "" {
 		name = "World"
 	}
-	if lanuage == "Spanish" {
+	if lanuage == spanish {
 		return spanishHelloPrefix + name
 	}
+
+	if lanuage == french {
+		return frenchHelloPrefix + name
+	}
+
 	return englishHelloPrefix + name
 }
 
