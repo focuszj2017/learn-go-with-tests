d4212df609cf2a515f37afa6a84706bfa97b8582
focus <junzhou21@126.comm>
2023-12-28 00:39:29 +0800

    Slices SumAll test

--- arrays/sum.go
+++ arrays/sum.go
@@ -7,3 +7,13 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumber := len(numbersToSum)
+	sums := make([]int, lengthOfNumber)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+	return sums
+}
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,7 @@
 package arrays
 
 import (
+	"reflect"
 	"slices"
 	"testing"
 )
@@ -30,4 +31,13 @@ func TestSum(t *testing.T) {
 		}
 	})
 
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
