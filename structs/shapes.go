dc091bd45df3bfe3b566060eda44126279ae3853
focus <junzhou21@126.comm>
2023-12-30 00:05:19 +0800

    add area method for shape_test

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,14 +1,24 @@
 package structs
 
+import "math"
+
 type Rectangle struct {
-	width  float64
-	height float64
+	Width  float64
+	Height float64
+}
+
+func (r *Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c *Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
 
 func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.height + rectangle.width)
-}
-
-func Area(rectangle Rectangle) float64 {
-	return (rectangle.height * rectangle.width)
+	return 2 * (rectangle.Height + rectangle.Width)
 }
