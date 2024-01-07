45c0f5e0bf18b5782871378af4e5ce8d703d3988
focus <junzhou21@126.comm>
2024-01-07 14:16:33 +0800

    mocking test

---
+++ mocking/countdown.go
@@ -0,0 +1,22 @@
+package mocking
+
+import (
+	"fmt"
+	"io"
+	"time"
+)
+
+const (
+	finalWord      = "Go!"
+	countdownStart = 3
+)
+
+func Countdown(out io.Writer) {
+	// fmt.Fprint(out, 3)
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		time.Sleep(1 * time.Second)
+	}
+
+	fmt.Fprint(out, finalWord)
+}
